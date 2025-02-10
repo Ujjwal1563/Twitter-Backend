dcb0be0631cfd9a21309124bb17040b3e8825d14
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-02-10 12:32:57 +0530

    Added Tweet basic Model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from 'dotenv';
-dotenv.config();
+import connect from "./config/database.js";
+import Tweet from "./models/tweet.js";
 const app = express();
-
 
 app.listen(3000, async () => {
   console.log("Server started at 3000");
-  mongoose.connect(process.env.URI);
+  connect();
   console.log("MongoDB connected");
+  Tweet.create({
+    content: "This is my first tweet",
+    likes: 25,
+    noOfRetweets: 5,
+    comment: "This is my first comment",
+  });
 });
